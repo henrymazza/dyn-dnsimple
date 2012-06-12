78444cfea60e1a4cff88bf9126d624b228dd7348
henrymazza <fabio.mazarotto@me.com>
2012-06-12 16:52:53 -0300

    Setting up logging, relative paths, etc

--- control.rb
+++ control.rb
@@ -2,5 +2,5 @@ require 'rubygems'
 require 'daemons'
 
 pwd = Dir.pwd
-Daemons.run('/home/fabio/dyn-dnsimple/dyn-dnsimple.rb', {:dir_mode => :normal, :dir => "/home/fabio/dyn-dnsimple/tmp/pids"}) 
+Daemons.run(File.join(File.dirname(__FILE__), 'dyn-dnsimple.rb'), {:dir_mode => :normal, :dir => File.join(File.dirname(__FILE__), "tmp/pids"), :app_name => "dyn-dnsimple", :backtrace => true, :log_output => true }) 
   
