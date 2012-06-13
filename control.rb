07104a20580c2202dde6c3e94a388a9ceda56683
Fabio Mazarotto <fabio.mazarotto@me.com>
2012-06-13 14:56:51 -0300

    Monitor process.
    Fancier formated Hash.

--- control.rb
+++ control.rb
@@ -2,5 +2,11 @@ require 'rubygems'
 require 'daemons'
 
 pwd = Dir.pwd
-Daemons.run(File.join(File.dirname(__FILE__), 'dyn-dnsimple.rb'), {:dir_mode => :normal, :dir => File.join(File.dirname(__FILE__), "tmp/pids"), :app_name => "dyn-dnsimple", :backtrace => true, :log_output => true }) 
-  
+Daemons.run(File.join(File.dirname(__FILE__), 'dyn-dnsimple.rb'),{
+  :dir_mode   => :normal,
+  :dir        => File.join(File.dirname(__FILE__), "tmp/pids"),
+  :app_name   => "dyn-dnsimple",
+  :backtrace  => true,
+  :log_output => true,
+  :monitor    => true
+})
